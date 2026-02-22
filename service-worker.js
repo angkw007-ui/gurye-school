be8d04d0b5e9dc31d5f01c0624b9a627002331f0
angkw007-ui <angkw007@gmail.com>
2026-02-22 21:12:56 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'gurye-school-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  'https://cdn.tailwindcss.com'
+];
+
+// 설치 - 파일 캐싱
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('캐시 열기 성공');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 활성화 - 오래된 캐시 삭제
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('오래된 캐시 삭제:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 네트워크 요청 처리
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 캐시 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // 오프라인일 때
+        return caches.match('/index.html');
+      })
+  );
+});
